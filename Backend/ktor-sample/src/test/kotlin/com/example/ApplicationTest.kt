d0c918448c5b48d8ae1ab45c23759cc03a98da9f
Ali Rahimi <1aliirahimi1@gmail.com>
2022-08-21 12:17:33 +0430

    search the hero that it is not in our server test added

--- Backend/ktor-sample/src/test/kotlin/com/example/ApplicationTest.kt
+++ Backend/ktor-sample/src/test/kotlin/com/example/ApplicationTest.kt
@@ -158,6 +158,20 @@ class ApplicationTest {
     }
 
 
+    @ExperimentalSerializationApi
+    @Test
+    fun `access search heroes endpoint, query non existing hero, assert empty list as a result`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes/search?name=unknown").apply {
+                assertEquals(expected = HttpStatusCode.OK, actual = response.status())
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                    .heroes
+                assertEquals(expected = emptyList(), actual = actual)
+            }
+        }
+    }
+
+
     private fun calculatePage(page: Int): Map<String, Int?> {
         var prevPage: Int? = page
         var nextPage: Int? = page
