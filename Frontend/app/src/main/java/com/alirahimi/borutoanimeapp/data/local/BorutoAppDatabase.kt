e6cf0288c20d49b18b8b5eb49adf1b0f6090cbe1
Ali Rahimi <1aliirahimi1@gmail.com>
2022-08-21 15:36:31 +0430

    creating new dao interface

--- Frontend/app/src/main/java/com/alirahimi/borutoanimeapp/data/local/BorutoAppDatabase.kt
+++ Frontend/app/src/main/java/com/alirahimi/borutoanimeapp/data/local/BorutoAppDatabase.kt
@@ -3,11 +3,16 @@ package com.alirahimi.borutoanimeapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.alirahimi.borutoanimeapp.data.local.data_access_object.HeroDao
+import com.alirahimi.borutoanimeapp.data.local.data_access_object.HeroRemoteKeyDao
 import com.alirahimi.borutoanimeapp.domain.model.Hero
+import com.alirahimi.borutoanimeapp.domain.model.HeroRemoteKey
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
 abstract class BorutoAppDatabase : RoomDatabase() {
 
     abstract fun heroDataAccessObject(): HeroDao
+
+    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
+
 
 }
