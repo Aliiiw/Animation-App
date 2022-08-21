a14162158e083f122942980d41610e459da13d30
Ali Rahimi <1aliirahimi1@gmail.com>
2022-08-21 15:55:41 +0430

    add the hilt and dagger

--- Frontend/app/src/main/java/com/alirahimi/borutoanimeapp/MainActivity.kt
+++ Frontend/app/src/main/java/com/alirahimi/borutoanimeapp/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.alirahimi.borutoanimeapp.navigation.SetupNavigationGraph
 import com.alirahimi.borutoanimeapp.ui.theme.BorutoAnimeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navigationController: NavHostController
