511188dec77d58ba813ff55d3a764d4d7d5034c5
Ali Rahimi <1aliirahimi1@gmail.com>
2022-08-21 17:04:20 +0430

    done

--- Frontend/app/src/main/java/com/alirahimi/borutoanimeapp/MyApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.alirahimi.borutoanimeapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MyApplication : Application()
--- Frontend/app/src/main/java/com/alirahimi/borutoanimeapp/utilities/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.alirahimi.borutoanimeapp.utilities
-
-object Constants {
-    const val DETAILS_ARGUMENT_KEY = "heroId"
-    const val HERO_DATABASE_TABLE = "hero_table"
-    const val HERO_REMOTE_KEY_DATABASE_TABLE = "hero_remote_key_table"
-    const val BORUTO_DATABASE = "boruto_database"
-}
