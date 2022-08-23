63c2da4c483a651efe2bd5cf4bad91e71f92a64d
Ali Rahimi <1aliirahimi1@gmail.com>
2022-08-23 15:03:12 +0430

    add the dark splash screen

--- Frontend/app/src/main/java/com/alirahimi/borutoapp/presentation/screens/splash/SplashScreen.kt
+++ Frontend/app/src/main/java/com/alirahimi/borutoapp/presentation/screens/splash/SplashScreen.kt
@@ -2,12 +2,14 @@ package com.alirahimi.borutoapp.presentation.screens.splash
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,21 +31,37 @@ fun SplashScreen(navigationController: NavHostController) {
 @Composable
 fun Splash() {
 
-    Box(
-        modifier = Modifier
-            .background(
-                Brush.verticalGradient(
-                    listOf(mainColor, mainColor2, mainColor3, mainColor4)
-                )
+    if (isSystemInDarkTheme()) {
+        Box(
+            modifier = Modifier
+                .background(Color.Black)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(R.string.app_logo)
             )
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
 
-        Image(
-            painter = painterResource(id = R.drawable.ic_logo),
-            contentDescription = stringResource(R.string.app_logo)
-        )
+        }
+    } else {
+        Box(
+            modifier = Modifier
+                .background(
+                    Brush.verticalGradient(
+                        listOf(mainColor, mainColor2, mainColor3, mainColor4)
+                    )
+                )
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
 
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(R.string.app_logo)
+            )
+
+        }
     }
 }
