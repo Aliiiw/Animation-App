e70e0487a106c7564824f9245f6ec094df0fba06
Ali Rahimi <1aliirahimi1@gmail.com>
2022-08-23 14:36:00 +0430

    add the converter for items in database

--- Frontend/app/src/main/java/com/alirahimi/borutoapp/data/local/BorutoAppDatabase.kt
+++ Frontend/app/src/main/java/com/alirahimi/borutoapp/data/local/BorutoAppDatabase.kt
@@ -2,12 +2,14 @@ package com.alirahimi.borutoapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.alirahimi.borutoapp.data.local.data_access_object.HeroDao
 import com.alirahimi.borutoapp.data.local.data_access_object.HeroRemoteKeyDao
 import com.alirahimi.borutoapp.domain.model.Hero
 import com.alirahimi.borutoapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class BorutoAppDatabase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
