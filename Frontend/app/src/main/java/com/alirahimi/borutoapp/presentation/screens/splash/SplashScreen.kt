d72932c8fb519bcaf6d25e4329797d7afcd54242
Ali Rahimi <1aliirahimi1@gmail.com>
2022-08-23 15:16:56 +0430

    change the status bar and animate the logo

--- Frontend/app/src/main/java/com/alirahimi/borutoapp/presentation/screens/splash/SplashScreen.kt
+++ Frontend/app/src/main/java/com/alirahimi/borutoapp/presentation/screens/splash/SplashScreen.kt
@@ -1,18 +1,22 @@
 package com.alirahimi.borutoapp.presentation.screens.splash
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.alirahimi.borutoapp.R
 import com.alirahimi.borutoapp.ui.theme.mainColor
@@ -23,13 +27,27 @@ import com.alirahimi.borutoapp.ui.theme.mainColor4
 @Composable
 fun SplashScreen(navigationController: NavHostController) {
 
-    Splash()
+    val rotate = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        rotate.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+    }
+
+    Splash(rotate = rotate.value)
 
 }
 
 
 @Composable
-fun Splash() {
+fun Splash(rotate: Float) {
 
     if (isSystemInDarkTheme()) {
         Box(
@@ -40,6 +58,7 @@ fun Splash() {
         ) {
 
             Image(
+                modifier = Modifier.rotate(degrees = rotate),
                 painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.app_logo)
             )
@@ -58,10 +77,10 @@ fun Splash() {
         ) {
 
             Image(
+                modifier = Modifier.rotate(degrees = rotate),
                 painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.app_logo)
             )
-
         }
     }
 }
