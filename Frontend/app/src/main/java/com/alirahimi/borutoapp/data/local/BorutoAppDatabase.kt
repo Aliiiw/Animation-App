ec2ba2f23a3f744cba1468e6c7cd269f32dabbe0
Ali Rahimi <1aliirahimi1@gmail.com>
2022-08-23 14:32:43 +0430

    add the remote key to room

--- Frontend/app/src/main/java/com/alirahimi/borutoapp/data/local/BorutoAppDatabase.kt
+++ Frontend/app/src/main/java/com/alirahimi/borutoapp/data/local/BorutoAppDatabase.kt
@@ -3,11 +3,14 @@ package com.alirahimi.borutoapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.alirahimi.borutoapp.data.local.data_access_object.HeroDao
+import com.alirahimi.borutoapp.data.local.data_access_object.HeroRemoteKeyDao
 import com.alirahimi.borutoapp.domain.model.Hero
+import com.alirahimi.borutoapp.domain.model.HeroRemoteKey
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
 abstract class BorutoAppDatabase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
+    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
 
 }
